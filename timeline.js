243a34bde59f59bf3683de39fc30b223a97344ff
LEE <bestesofficial@gmail.com>
2026-01-18 15:17:41 +0900

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -31,7 +31,7 @@ function setupTimeline() {
 
     const v = getModelsCacheKey();
     const baseUrl = getModelsBaseUrl();
-    const url = `${baseUrl}/models.json` + (v ? `?v=${encodeURIComponent(v)}` : '');
+    const url = `${baseUrl}/models.json` ; //fucking Vtag
 
     window.__PGM_MODELS_DATA_PROMISE__ = fetch(url, { cache: 'no-store' })
       .then((r) => {
@@ -130,3 +130,4 @@ function setupTimeline() {
 
   renderTimelineFromModels();
 }
+
